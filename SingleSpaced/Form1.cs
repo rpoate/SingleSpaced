0a1a9bce7e33f8f2f7d9ae35072129d89b984ae5
Richard Poate <info@zoople.com.au>
2022-05-30 19:02:57 +1000

    Add project files.

---
+++ SingleSpaced/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SingleSpaced
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.htmlEditControl1.CSSText = "body {font-family: Arial} p {margin-top: 0; margin-bottom: 0}";
+            this.htmlEditControl1.ToolStripItems.Add("Show HTML").Click += ShowCode;
+        }
+
+        private void ShowCode(object sender, EventArgs e)
+        {
+            MessageBox.Show(this.htmlEditControl1.DocumentHTML);
+        }
+
+        private void htmlEditControl1_BeforeGetDocumentHTML(object sender, Zoople.BeforeGetDocumentHTMLEventArgs e)
+        {
+            e.htmlSource = e.htmlSource.Replace("<p", "<div");
+            e.htmlSource = e.htmlSource.Replace("/p>", "/div>");
+        }
+    }
+}
